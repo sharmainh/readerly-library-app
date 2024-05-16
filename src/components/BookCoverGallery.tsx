3e0be75323cde38fd1c0726e8b2774330ebe32f8
Sharmain <sharmainhenderson@Sharmains-MacBook-Air.local>
2024-05-16 10:25:22 -0700

    Update Book Cover Gallery: New Images Added

--- src/components/BookCoverGallery.tsx
+++ src/components/BookCoverGallery.tsx
@@ -1,6 +1,6 @@
 import digital from "../assets/images/books/Digital Marketing Strategy.jpg"
 import burst from "../assets/images/books/Bursts of Brilliance.jpg"
-import brand from "../assets/images/books/Brand Up.jpg"
+import brandup from "../assets/images/books/Brand Up.jpg"
 import circus from "../assets/images/books/Night Circus.png"
 import good from "../assets/images/books/The Good Life.jpeg"
 import money from "../assets/images/books/The Psychology of Money.png"
@@ -15,7 +15,7 @@ function BookCoverGallery() {
         <img src={circus} alt="" className="h-30 book-cover"/>
       </div>
       <div>
-        <img src={brand} alt="" className="mt-[10em] book-cover"/>
+        <img src={brandup} alt="" className="mt-[10em] book-cover"/>
       </div>
       <div>
         <img src={burst} alt="" className="book-cover"/>
