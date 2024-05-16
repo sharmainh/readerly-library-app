5b0e5b9d5d58a28033db29545e3c550f1b30851a
Sharmain <sharmainhenderson@Sharmains-MacBook-Air.local>
2024-05-16 10:15:39 -0700

    Update Book Cover Gallery: New Images Added

--- src/components/BookCoverGallery.tsx
+++ src/components/BookCoverGallery.tsx
@@ -1,33 +1,33 @@
+import digital from "../assets/images/books/Digital Marketing Strategy.jpg"
+import burst from "../assets/images/books/Bursts of Brilliance.jpg"
+import brand from "../assets/images/books/Brand Up.jpg"
 import circus from "../assets/images/books/Night Circus.png"
+import good from "../assets/images/books/The Good Life.jpeg"
 import money from "../assets/images/books/The Psychology of Money.png"
-import outsider from "../assets/images/books/The Outsider.png"
-import ottoman from "../assets/images/books/Ottoman.png"
-import forest from "../assets/images/books/Shadow in the Forest.png"
-import hunter from "../assets/images/books/The Forest Hunter.png"
 
 
 function BookCoverGallery() {
   return (
     <div className="grid grid-rows-2 gap-5 grid-flow-col max-h-[75px] 
-    ml-64 mt-16 w-2/5 h-32"
+    ml-64 mt-14 w-2/5 h-32"
     >
       <div>
-        <img src={money} alt="" className="h-30 book-cover"/>
+        <img src={circus} alt="" className="h-30 book-cover"/>
       </div>
       <div>
-        <img src={outsider} alt="" className="mt-[7.5em] book-cover"/>
+        <img src={brand} alt="" className="mt-[10em] book-cover"/>
       </div>
       <div>
-        <img src={ottoman} alt="" className="book-cover"/>
+        <img src={burst} alt="" className="book-cover"/>
       </div>
       <div>
-        <img src={forest} alt="" className="mt-[8.5em] book-cover"/>
+        <img src={good} alt="" className="mt-[10em] book-cover"/>
       </div> 
       <div>
-        <img src={hunter} alt="" className="book-cover"/>
+        <img src={digital} alt="" className="book-cover"/>
       </div>
       <div>
-        <img src={circus} alt="" className="mt-[6em] book-cover"/>
+        <img src={money} alt="" className="mt-[10em] book-cover"/>
       </div>       
     </div>
   )
